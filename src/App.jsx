62f6ee36e7ed04c798f33e8c71a6b045ade9cfd2
Yash Srivastava <ysyashmks@gmail.com>
2025-03-20 23:07:58 +0530

    refactor: remove Start page from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Home from './Pages/Home'
 import About from './Pages/About'
-import Start from './Pages/Start'
+
 
 function App() {
   return (
     <div>
       <Home />
       <About/>
-      {/* <Start /> */}
     </div>
   )
 }
